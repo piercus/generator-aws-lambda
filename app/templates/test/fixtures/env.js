467f5d8252eb80b40a4641991bf6e7bfa8f69643
Pierre Colle <piercus@gmail.com>
2017-01-31 13:27:21 +0100

    region rm from env.js

--- app/templates/test/fixtures/env.js
+++ app/templates/test/fixtures/env.js
@@ -5,4 +5,3 @@ const cfg = require('../../config.json');
 process.env.TZ = 'UTC';
 process.env.NODE_ENV = 'test';
 process.env.AWS_LAMBDA_FUNCTION_NAME = `test_${pkg.name}`;
-process.env.AWS_REGION = cfg.region;
